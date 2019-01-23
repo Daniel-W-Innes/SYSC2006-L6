1b4c38b90e8ed17150e47dd708cd85a4168cdd66
Daniel Innes <Daniel.W.Innes@gmail.com>
2019-01-23 09:31:54 -0500

    upload

---
+++ Lab 6/cmake-build-debug/CMakeFiles/Lab_6.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_6.dir/array_list.c.obj"
+  "CMakeFiles/Lab_6.dir/main.c.obj"
+  "Lab_6.pdb"
+  "Lab_6.exe"
+  "Lab_6.exe.manifest"
+  "libLab_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
